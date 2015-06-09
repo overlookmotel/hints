b8610bf3015c6bc87e3a12663acde9771e164f2d
overlookmotel <theoverlookmotel@gmail.com>
2015-06-09 04:17:04 +0100

    Initial release

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # hints.js
 
-# Tools for parsing hint comments in code
+# Tools for parsing hint comments in Javascript code
 
 ## Current status
 
@@ -10,7 +10,70 @@
 [![Dev dependency Status](https://img.shields.io/david/dev/overlookmotel/hints.svg)](https://david-dm.org/overlookmotel/hints)
 [![Coverage Status](https://img.shields.io/coveralls/overlookmotel/hints/master.svg)](https://coveralls.io/r/overlookmotel/hints)
 
+API is not stable yet. May change in v0.2.0.
+
 ## Usage
+
+Parses Javascript code for hint comments.
+
+#### `hints(code, identifier [, options])`
+
+```js
+function x(a, b) {
+    // testhint a:1 b.c:2 b.d e
+}
+
+var hints = require('hints');
+var h = hints(x.toString(), 'testhint');
+// h = {a: 1, b: {c: 2, d: true}, e: true}
+```
+
+#### `hints.full(code, identifier [, options])`
+
+Uses [acorn](https://www.npmjs.com/package/acorn) internally to parse the code. You can get the AST (abstract syntax tree) returned too.
+
+```js
+function x(a, b) {
+    // testhint a:1
+}
+
+var result = hints.full(x.toString(), 'testhint');
+// result.hints = {a: 1}
+// result.hintsPos = {a: {value: 1, start: 20, end: 36}}
+// result.tree = { /* AST */ }
+```
+
+### Options
+
+#### `pos`
+
+Returns character positions of the comments with the hints.
+Default: `false`
+
+```js
+function x(a, b) {
+    // testhint a:1
+}
+
+var h = hints(x.toString(), 'testhint', {pos: true});
+// h = {a: {value: 1, start: 20, end: 36}}
+```
+
+#### function
+
+If parsing the code of a function, set this option as `true`.
+Default: `false`
+
+```js
+var x = function (a, b) {
+    // testhint a:1
+}
+
+var h = hints(x.toString(), 'testhint', {function: true});
+// h = {a: {value: 1, start: 20, end: 36}}
+```
+
+NB Without `function: true` set, the above would throw an error as [acorn](https://www.npmjs.com/package/acorn) sees a function statement with no function name - which is illegal.
 
 ## Tests
 
--- test/all.test.js
+++ test/all.test.js
@@ -16,8 +16,83 @@ chai.config.includeStack = true;
 /* jshint expr: true */
 /* global describe, it */
 
-describe('Tests', function() {
-	it('Test', function() {
-		expect(hints).to.be.ok;
+describe('hints()', function() {
+	describe('Single-line comments', function() {
+		var comment1 = '//  testhint a.b:1 c:3 d \t\n',
+			comment2 = '//   testhint e\n';
+		var code = 'function x(a, b) {\n' +
+			'\t' + comment1 +
+			'  ' + comment2 +
+			'}';
+
+		it('finds hints', function() {
+			var res = hints(code, 'testhint');
+
+			expect(res).to.deep.equal({ a: { b: '1' }, c: '3', d: true, e: true });
+		});
+
+		it('pos option', function() {
+			var res = hints(code, 'testhint', {pos: true});
+
+			expect(code.slice(res.a.b.start, res.a.b.end)).to.equal(comment1);
+			expect(code.slice(res.c.start, res.c.end)).to.equal(comment1);
+			expect(code.slice(res.d.start, res.d.end)).to.equal(comment1);
+			expect(code.slice(res.e.start, res.e.end)).to.equal(comment2);
+		});
+	});
+
+	describe('Multi-line comments', function() {
+		var comment1 = '/*testhint a.b:1 c:3 \t \n' +
+			' * foo foo \n' +
+			' *\t  *testhint d\n' +
+			' */';
+		var comment2 = '/*testhint e*/';
+		var code = 'function x(a, b) {\n' +
+			comment1 + '\n' +
+			comment2 + 'alert(); // foo\n' +
+			'}';
+
+		it('finds hints', function() {
+			var res = hints(code, 'testhint');
+
+			expect(res).to.deep.equal({ a: { b: '1' }, c: '3', d: true, e: true });
+		});
+
+		it('pos option', function() {
+			var res = hints(code, 'testhint', {pos: true});
+
+			expect(code.slice(res.a.b.start, res.a.b.end)).to.equal(comment1);
+			expect(code.slice(res.c.start, res.c.end)).to.equal(comment1);
+			expect(code.slice(res.d.start, res.d.end)).to.equal(comment1);
+			expect(code.slice(res.e.start, res.e.end)).to.equal(comment2);
+		});
+	});
+});
+
+describe('hints.full()', function() {
+	it('returns hints, hintsPos and tree', function() {
+		var comment1 = '// testhint a\n';
+		var code = 'function x(a, b) {\n' + comment1 + '}';
+
+		var res = hints.full(code, 'testhint');
+
+		expect(res.hints).to.deep.equal({a: true});
+		expect(res.hintsPos).to.be.ok;
+		expect(code.slice(res.hintsPos.a.start, res.hintsPos.a.end)).to.equal(comment1);
+		expect(res.tree).to.be.ok;
+		expect(res.tree.body).to.be.ok;
+	});
+
+	it('function option', function() {
+		var comment1 = '// testhint a\n';
+		var code = 'function (a, b) {\n' + comment1 + '}';
+
+		var res = hints.full(code, 'testhint', {pos: true, function: true});
+
+		expect(res.hints).to.deep.equal({a: true});
+		expect(res.hintsPos).to.be.ok;
+		expect(code.slice(res.hintsPos.a.start, res.hintsPos.a.end)).to.equal(comment1);
+		expect(res.tree).to.be.ok;
+		expect(res.tree.body).to.be.ok;
 	});
 });
